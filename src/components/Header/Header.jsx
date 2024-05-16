c792cb0671771e630725231d78ed839059cc4393
arafatrahman41 <arafatrahman41@outlook.com>
2024-05-16 15:32:20 +0600

    added some more thing

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,15 +1,16 @@
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
+// Link 
 import './Header.css'
 const Header = () => {
     return (
         <div>
             <nav className="header">
                 <p>My website</p>
-                <Link to="/">Home</Link>
-                <Link to="users">Users</Link>
-                <Link to="posts">Posts</Link>
-                <Link to="about">About</Link>
-                <Link to="/contact">Contact us</Link>
+                <NavLink to='/'>Home</NavLink>
+                <NavLink to='/users'>Users</NavLink>
+                <NavLink to='/posts'>Posts</NavLink>
+                <NavLink to='/about'>About</NavLink>
+                <NavLink to='/contact'>Contact Us</NavLink>
             </nav>
         </div>
     );
